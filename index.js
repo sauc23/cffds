952616db37a97922e524cea3dbb75028f20bc969
bruh <89742348+sauc23@users.noreply.github.com>
2025-07-28 00:58:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,69 +2,58 @@ const http = require('http');
 const https = require('https');
 const { URL } = require('url');
 
+const BASE_DOMAIN = "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net";
+
 const PROXY_CONFIG = {
-  "ufc-313": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/9a/3v6aia/",
-  "ufc-314": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/db/n8y855/",
-  "ufc-316": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/ek/tarrn3/",
-  "ufc-317": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/gee/jcuc5g/",
-  "ufc-318": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/xq/6snqfn/",
-  "wwe-nxt-new-years-evil-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/js/un5ti1/",
-  "wwe-royal-rumble-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/xy/3axbnz/",
-  "wwe-night-of-champions-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/gee/ojv5gq/",
-  "aew-dynasty-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/61/hlc93j/",
-  "aew-all-in-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/vz1/pucoym/",
-  "ucl-final-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/vz1/n8l9yq/",
-  "ucl-final-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/vz1/n8l9yq/",
-  "2025-nba-finals-game-1": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/6hp/jc9uyr/",
-  "2025-nba-finals-game-2": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/mf/ureswn/",
-  "2025-nba-finals-game-3": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/6hp/vzj8se/",
-  "2025-nba-finals-game-4": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/xy/x6aw9i/",
-  "2025-nba-finals-game-5": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/5c/8tuofv/",
-  "2025-nba-finals-game-6": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/5c/pkmgqd/",
-  "2025-nba-finals-game-7": "hhttps://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/vz1/isy9py/",
-  "ring-iii": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/8q/puccd9/",
-  "usyk-vs-dubois-ii": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/rw/6snqub/",
-  "pacquiao-vs-barrios": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/ho/85xgw8/",
-  "evw18": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/xy/gezbw3/",
-  "paul-vs-chavezjr": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/pp/luik6o/",
-  "wilder-vs-herndon": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/9a/pk69zz/",
-  "f1-2025-australia-gp": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/6hp/3vny3q/",
-  "f1-2025-china-gp": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/bgm/hlmnkd/",
-  "f1-2025-japan-gp": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/gee/pk6qui/",
-  "f1-2025-bahrain-gp": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/gee/jcuz85/",
-  "f1-2025-saudi-arabia-gp": "http://spok.feistjmyrodne.sbs/v4/xy/pk6qk6/",
-  "f1-2025-miami-gp": "https://sxix.paxtoncloudou.sbs/v4/pp/q3o15b/",
-  "f1-2025-imola-gp": "https://sqtd.disalvozunigam.sbs/v4/pp/pk6qoz/",
-  "f1-2025-monaco-gp": "https://slob.performanceactivation.shop/v4/pp/vz8gfd/",
-  "f1-2025-spain-gp": "https://sqtd.forecastinggalactic.shop/v4/mf/mi1ke1/",
-  "f1-2025-canada-gp": "https://s8v3.encryptiontransmit.cyou/v4/vz1/6hyurg/",
-  "f1-2025-austria-gp": "https://s8v3.boehmerrbfjx.sbs/v4/bgm/5wqzoy/",
-  "f1-2025-britan-gp": "https://snc9.cryptologydashboard.sbs/v4/6hp/dqcoje/",
-  "back-to-the-beginning-2025": "https://ftryr4e535tyujhgfdsx.global.ssl.fastly.net/v4/8q/qtzz5o/",
-  "zlthlchlwlmuqimu4isp": "https://spok.visualizeencryption.sbs/v4/6hp/xildkx/",
+  "ufc-313": "/v4/9a/3v6aia/",
+  "ufc-314": "/v4/db/n8y855/",
+  "ufc-316": "/v4/ek/tarrn3/",
+  "ufc-317": "/v4/gee/jcuc5g/",
+  "ufc-318": "/v4/xq/6snqfn/",
+  "wwe-nxt-new-years-evil-2025": "/v4/js/un5ti1/",
+  "wwe-royal-rumble-2025": "/v4/xy/3axbnz/",
+  "wwe-night-of-champions-2025": "/v4/gee/ojv5gq/",
+  "aew-dynasty-2025": "/v4/61/hlc93j/",
+  "aew-all-in-2025": "/v4/vz1/pucoym/",
+  "ucl-final-2025": "/v4/vz1/n8l9yq/",
+  "2025-nba-finals-game-1": "/v4/6hp/jc9uyr/",
+  "2025-nba-finals-game-2": "/v4/mf/ureswn/",
+  "2025-nba-finals-game-3": "/v4/6hp/vzj8se/",
+  "2025-nba-finals-game-4": "/v4/xy/x6aw9i/",
+  "2025-nba-finals-game-5": "/v4/5c/8tuofv/",
+  "2025-nba-finals-game-6": "/v4/5c/pkmgqd/",
+  "2025-nba-finals-game-7": "/v4/vz1/isy9py/",
+  "ring-iii": "/v4/8q/puccd9/",
+  "usyk-vs-dubois-ii": "/v4/rw/6snqub/",
+  "pacquiao-vs-barrios": "/v4/ho/85xgw8/",
+  "evw18": "/v4/xy/gezbw3/",
+  "paul-vs-chavezjr": "/v4/pp/luik6o/",
+  "wilder-vs-herndon": "/v4/9a/pk69zz/",
+  "f1-2025-australia-gp": "/v4/6hp/3vny3q/",
+  "f1-2025-china-gp": "/v4/bgm/hlmnkd/",
+  "f1-2025-japan-gp": "/v4/gee/pk6qui/",
+  "f1-2025-bahrain-gp": "/v4/gee/jcuz85/",
+  "back-to-the-beginning-2025": "/v4/8q/qtzz5o/",
+  "zlthlchlwlmuqimu4isp": "/v4/6hp/xildkx/"
 };
 
 http.createServer((req, res) => {
   const reqUrl = new URL(req.url, `http://${req.headers.host}`);
   const pathname = reqUrl.pathname;
 
-  // Find matching key
   const key = Object.keys(PROXY_CONFIG).find(k => pathname.startsWith(`/${k}/`));
   if (!key) {
     res.writeHead(404, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
     return res.end('Not found');
   }
 
-  const baseUrl = PROXY_CONFIG[key];
   let proxyPath = pathname.replace(`/${key}/`, '');
-
   if (proxyPath === 'video.m3u8') {
     proxyPath = 'cf-master.txt';
   }
 
-  const targetUrl = new URL(proxyPath, baseUrl);
-  const isHttps = targetUrl.protocol === 'https:';
-  const lib = isHttps ? https : http;
+  const targetUrl = new URL(PROXY_CONFIG[key] + proxyPath, BASE_DOMAIN);
+  const lib = https;
 
   const options = {
     method: req.method,
